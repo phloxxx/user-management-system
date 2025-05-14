58c39be694f5cb9b9b9d1e0860cc8654c5e42748
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-14 14:22:05 +0800

    added yaml and serverjs to root

---
+++ server.js
@@ -0,0 +1,29 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('_middleware/error-handler');
+
+app.use(cors({
+  origin: 'http://localhost:4200', // Angular app URL
+  credentials: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// api routes
+app.use('/accounts', require('./accounts/account.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('_helpers/swagger'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
