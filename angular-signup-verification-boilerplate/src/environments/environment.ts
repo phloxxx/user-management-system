eaf72e0be6ed24185bbca75434c0e9fce3cbbcca
darkensteve <rovic.steve@gmail.com>
2025-05-17 16:55:42 +0800

    fix the frontend and put the apiURL of the render

--- angular-signup-verification-boilerplate/src/environments/environment.ts
+++ angular-signup-verification-boilerplate/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000'
+  // apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://real-user-management-system.onrender.com' // Replace with your actual deployment URL
 };
 
 /*
