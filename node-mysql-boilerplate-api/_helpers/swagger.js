65f3c771f43fec6341098c604e9217d77e8ecf4b
darkensteve <rovic.steve@gmail.com>
2025-05-17 17:12:22 +0800

    initial commit

--- node-mysql-boilerplate-api/_helpers/swagger.js
+++ node-mysql-boilerplate-api/_helpers/swagger.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const path = require('path');
+
+// Use path.join to create an absolute path to the swagger.yaml file
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
