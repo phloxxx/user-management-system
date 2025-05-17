11a1fb7af09156a3243055a05748ca401d641564
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-17 18:09:39 +0800

    changed build

--- node-mysql-boilerplate-api/server.js
+++ node-mysql-boilerplate-api/server.js
@@ -121,7 +121,6 @@ const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
-
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (error) => {
     console.error('Unhandled promise rejection:', error);
