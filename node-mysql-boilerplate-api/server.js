e9d281a5bcd0fa5527f76b157732137b3d27eac0
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-19 01:10:16 +0800

    returned frontend url

--- node-mysql-boilerplate-api/server.js
+++ node-mysql-boilerplate-api/server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./_middleware/error-handler');
 
 
 app.use(cors({
-  origin: 'https://ocliasa-user-management-system.onrender.com',
+  origin: 'https://ocliasa-user-management-system0.onrender.com',
   credentials: true
 }));
 
