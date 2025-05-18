531d2d5cd98cbb1b1a9bc794a134d53ac8b60ad0
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-19 01:33:28 +0800

    reverted email

--- node-mysql-boilerplate-api/_helpers/send-email.js
+++ node-mysql-boilerplate-api/_helpers/send-email.js
@@ -4,19 +4,6 @@ const config = require('../config.json');
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    try {
-        console.log('Attempting to send email with config:', {
-            host: config.smtpOptions.host,
-            port: config.smtpOptions.port,
-            user: config.smtpOptions.auth.user
-        });
-        
-        const transporter = nodemailer.createTransport(config.smtpOptions);
-        const result = await transporter.sendMail({ from, to, subject, html });
-        console.log('Email sent successfully to:', to);
-        return result;
-    } catch (error) {
-        console.error('Failed to send email:', error.message);
-        throw error;
-    }
+    const transporter = nodemailer.createTransport(config.smtpOptions);
+    await transporter.sendMail({ from, to, subject, html });
 }
