27357d7b6d045467e3ab1a1599bae61effb54d25
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-19 01:23:51 +0800

    changed email

--- node-mysql-boilerplate-api/_helpers/send-email.js
+++ node-mysql-boilerplate-api/_helpers/send-email.js
@@ -4,6 +4,19 @@ const config = require('../config.json');
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html });
+    try {
+        console.log('Attempting to send email with config:', {
+            host: config.smtpOptions.host,
+            port: config.smtpOptions.port,
+            user: config.smtpOptions.auth.user
+        });
+        
+        const transporter = nodemailer.createTransport(config.smtpOptions);
+        const result = await transporter.sendMail({ from, to, subject, html });
+        console.log('Email sent successfully to:', to);
+        return result;
+    } catch (error) {
+        console.error('Failed to send email:', error.message);
+        throw error;
+    }
 }
