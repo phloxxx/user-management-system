54b5f6c4d524a9944fae148665481f0e2975e676
Nnzo8 <ninorollaneocliasa684@gmail.com>
2025-05-18 23:23:13 +0800

    sendemail

--- node-mysql-boilerplate-api/_helpers/send-email.js
+++ node-mysql-boilerplate-api/_helpers/send-email.js
@@ -1,9 +1,38 @@
 const nodemailer = require('nodemailer');
-const config = require('config.json');
+// Replace config import with proper path
+const config = require('../config.json');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html });
+    try {
+        // Create Ethereal test account
+        const testAccount = await nodemailer.createTestAccount();
+        
+        // Create transporter with Ethereal credentials
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass
+            }
+        });
+
+        // Send mail and get info
+        const info = await transporter.sendMail({ from, to, subject, html });
+        
+        // Log the preview URL
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        
+        return {
+            success: true,
+            messageId: info.messageId,
+            previewUrl: nodemailer.getTestMessageUrl(info)
+        };
+    } catch (error) {
+        console.error('Email sending failed:', error);
+        throw error;
+    }
 }
